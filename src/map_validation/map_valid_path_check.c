fe3209765fe6c461baaf76f2000a9f75fb57c643
Alina <ralcevichalina70@gmail.com>
2025-03-04 18:51:55 +0100

    Update

--- src/map_validation/map_valid_path_check.c
+++ src/map_validation/map_valid_path_check.c
@@ -6,99 +6,69 @@
 /*   By: apple <apple@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 12:59:35 by apple             #+#    #+#             */
-/*   Updated: 2025/03/04 10:25:45 by apple            ###   ########.fr       */
+/*   Updated: 2025/03/04 18:44:41 by apple            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-int	check_the_path(t_mlx *mlx, t_state state, int x, int y)
+int	dfs(char **grid, t_state state, int y, int x)
 {
-	while ((x >= 1 && y >= 1) && (x < state.rows && y < state.cols))
+	if (x < 0 || x >= state.rows || y < 0 || y >= state.cols
+		|| grid[y][x] == '1' || grid[y][x] == 'V')
 	{
-		if (state.grid[x][y] == '1')
-		{
-			ft_printf("Error: the map doesn't have a valid path.\n");
-			mlx_destroy_display(mlx->mlx_ptr);
-			free_map(&mlx->data);
-			free(mlx);
-			exit(1);
-		}
-		if (state.grid[x][y] == 'E')
-			return (1);
-		if (dfs(mlx, state, x - 1, y))
-			return (1);
-		if (dfs(mlx, state, x + 1, y))
-			return (1);
-		if (dfs(mlx, state, x, y - 1))
-			return (1);
-		if (dfs(mlx, state, x, y + 1))
-			return (1);
 		return (0);
 	}
-	return (1);
+	if (grid[y][x] == 'E')
+		return (1);
+	grid[y][x] = 'V';
+	if (dfs(grid, state, y - 1, x) || dfs(grid, state, y + 1, x)
+		|| dfs(grid, state, y, x - 1) || dfs(grid, state, y, x + 1))
+		return (1);
+	return (0);
 }
 
-int	dfs(t_mlx *mlx, t_state state, int x, int y)
-{
-	if (x < 0 || y < 0 || x >= state.rows || y >= state.cols)
-	{
-		ft_printf("Error: the map doesn't have a valid path.\n");
-		mlx_destroy_display(mlx->mlx_ptr);
-		free_map(&mlx->data);
-		free(mlx);
-		return (0);
-	}
-	return (check_the_path(mlx, state, x, y));
-}
-
-char	**map_to_arr(t_mlx *mlx, t_map *head, t_state state)
+char	**map_to_arr(t_mlx *mlx, t_state state)
 {
 	char	**grid;
-	char	**temp;
+	t_map	*temp;
 	int		j;
 
-	grid = malloc(sizeof(char *) * state.cols);
+	j = 0;
+	grid = malloc(sizeof(char *) * state.cols + 1);
 	if (!grid)
 	{
 		ft_printf("Grid is not allocated.\n");
-		mlx_destroy_display(mlx->mlx_ptr);
-		free_map(&mlx->data);
-		free(mlx);
 		return (NULL);
 	}
-	temp = grid;
-	j = 0;
-	while (head && j < state.cols)
+	temp = mlx->data;
+	while (temp)
 	{
-		temp[j] = ft_strdup(head->data);
+		grid[j] = ft_strdup(temp->data);
 		j++;
-		head = head->next;
+		temp = temp->next;
 	}
+	grid[j] = NULL;
 	return (grid);
 }
 
-void	valid_path(t_mlx *mlx, t_state state)
+int	valid_path(t_mlx *mlx, t_state state)
 {
-	state.x = 0;
-	state.y = 0;
-	state.grid = map_to_arr(mlx, mlx->data, state);
-	if (!state.grid)
+	char	**grid;
+	int		x;
+	int		y;
+
+	grid = map_to_arr(mlx, state);
+	if (!grid)
+		return (0);
+	find_coords(mlx, &x, &y, 'P');
+	if (!dfs(grid, state, y, x))
 	{
-		ft_printf("Error: the map is empty.\n");
-		mlx_destroy_display(mlx->mlx_ptr);
+		ft_printf("Error: the map doesn't have a valid path.\n");
+		free(grid);
 		free_map(&mlx->data);
 		free(mlx);
 		exit(1);
 	}
-	if (!find_coords(mlx, &state.x, &state.y, 'P'))
-	{
-		ft_printf("Player doesn't exist.\n");
-		free_grid(state.grid, state.cols);
-		mlx_destroy_display(mlx->mlx_ptr);
-		free_map(&mlx->data);
-		free(mlx);
-	}
-	dfs(mlx, state, state.x, state.y);
-	free_grid(state.grid, state.cols);
+	return (0);
 }
